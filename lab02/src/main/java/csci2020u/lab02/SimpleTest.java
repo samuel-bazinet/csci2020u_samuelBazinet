a3e4ed2120cd314599a13b2921fd37746ab3cee5
Sam B <bazinet.samuel1@gmail.com>
2021-01-26 12:23:23 -0500

    Completed part 1 of lab02

---
+++ lab02/src/main/java/csci2020u/lab02/SimpleTest.java
@@ -0,0 +1,9 @@
+package csci2020u.lab02;
+
+public class SimpleTest {
+
+    public static void main(String args[]) {
+        System.out.println("Hello from SimpleTest.java in the lab02 directory");
+    }
+
+}
