f3a1125cd800a8751d88c957219dc0e92041e18f
Sam B <bazinet.samuel1@gmail.com>
2021-02-23 22:29:54 -0500

    Completed the bonus part of lab 05

--- lab05/src/main/java/Lab05.java
+++ lab05/src/main/java/Lab05.java
@@ -1,8 +1,14 @@
+import javax.swing.Action;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.collections.*;
 
@@ -17,6 +23,12 @@ public class Lab05 extends Application {
         primaryStage.setTitle("Lab04");
 
         BorderPane border = new BorderPane();
+        GridPane grid = new GridPane();
+
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(20);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
 
         table.setEditable(true);
 
@@ -56,7 +68,49 @@ public class Lab05 extends Application {
 
         border.setTop(table);
 
-        primaryStage.setScene(new Scene(border, 700, 300));
+        Label labSID = new Label("SID:");
+        Label labAssig = new Label("Assingment:");
+        Label labMid = new Label("Midterm:");
+        Label labFE = new Label("Final Exam:");
+
+        TextField textSID = new TextField();
+        TextField textAssig = new TextField();
+        TextField textMid = new TextField();
+        TextField textFE = new TextField();
+
+        textSID.setPromptText("SID");
+        textAssig.setPromptText("Assignment");
+        textMid.setPromptText("Midterm");
+        textFE.setPromptText("Final Exam");
+
+        Button btnAdd = new Button("Add");
+
+        btnAdd.setOnAction(new EventHandler<ActionEvent>(){
+                @Override
+                public void handle(ActionEvent event) {
+                        String newSID = textSID.getText();
+                        float newAssig = Float.parseFloat(textAssig.getText());
+                        float newMid = Float.parseFloat(textMid.getText());
+                        float newFE = Float.parseFloat(textFE.getText());
+
+                        marks.add(new StudentRecord(newSID, newAssig, newMid, newFE));
+                }
+        });
+        
+        grid.add(labSID, 0, 0);
+        grid.add(labAssig, 2, 0);
+        grid.add(labMid, 0, 1);
+        grid.add(labFE, 2, 1);
+
+        grid.add(textSID, 1, 0);
+        grid.add(textAssig, 3, 0);
+        grid.add(textMid, 1, 1);
+        grid.add(textFE, 3, 1);
+        grid.add(btnAdd, 1, 2);
+
+        border.setCenter(grid);
+
+        primaryStage.setScene(new Scene(border, 700, 600));
 
         primaryStage.show();
 
